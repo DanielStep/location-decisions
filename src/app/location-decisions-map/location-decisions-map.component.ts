3d2fa69c42ea889fce8c96e1fa07767d13dd2229
Daniel Stepanenko <daniel.stepanenko@moula.com.au>
2023-10-17 18:06:45 +1100

    Try basic polygon overlay functionality

--- src/app/location-decisions-map/location-decisions-map.component.ts
+++ src/app/location-decisions-map/location-decisions-map.component.ts
@@ -6,28 +6,54 @@ import * as leaflet from 'leaflet';
   templateUrl: './location-decisions-map.component.html',
   styleUrls: ['./location-decisions-map.component.css'],
 })
-
 export class LocationDecisionsMapComponent implements AfterViewInit {
-
   private map!: leaflet.Map;
 
-  constructor() { }
+  // Sample data
+  private polygonsData = [
+    {
+      name: 'Polygon 1',
+      coordinates: [
+        { lat: -37.7950, lng: 144.9834 },
+        { lat: -37.7950, lng: 144.9934 },
+        { lat: -37.8100, lng: 144.9934 },
+        { lat: -37.8100, lng: 144.9834 }
+      ],
+    },
+    // ... other polygons
+  ];
+
+  constructor() {}
 
   ngAfterViewInit(): void {
     this.initMap();
+    this.addPolygons();
   }
 
   private initMap(): void {
     this.map = leaflet.map('map', {
-      center: [20, 0],
-      zoom: 2
+      center: [-37.8136, 144.9631],
+      zoom: 12,
     });
 
-    const tiles = leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
+    const tiles = leaflet.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }
+    );
 
     tiles.addTo(this.map);
   }
+
+  private addPolygons(): void {
+    this.polygonsData.forEach(polygonData => {
+      const latLngs = polygonData.coordinates.map(coord => new leaflet.LatLng(coord.lat, coord.lng));
+      const polygon = leaflet.polygon(latLngs).addTo(this.map);
+      polygon.bindPopup(polygonData.name);
+    });
+  }
+  
 }
